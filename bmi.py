f255c8d5fbb73e754f7fb36c42985c1e88b29bbc
nienlinh <nlhsueh@mail.fcu.edu.tw>
2024-05-11 21:18:57 +0800

    .

--- bmi.py
+++ bmi.py
@@ -1,8 +1,13 @@
-w = input('Please input the weight ')
-h = input('Please input the height ')
+print ("Please input weight and height, -999 to stop")
+w = input('Please input the weight (kg) ')
+h = input('Please input the height (m) ')
+w, h = float(w), float(h)
 bmi_set = []
 while w != -999 and h != -999:
- bmi = w/(h*h)
- bmi_set.add(bmi)
+  bmi = round(w/(h*h),1)
+  bmi_set.append(bmi)
+  w = float(input('Please input the weight (kg) '))
+  h = float(input('Please input the height (m) '))
+
 for b in bmi_set:
- print (b)
+ print ('BMI:', b)

e1238a2459b0ab1f5f56536d4991bf5ea11778f8
nienlinh <nlhsueh@mail.fcu.edu.tw>
2024-05-11 21:21:29 +0800

    .

--- bmi.py
+++ bmi.py
@@ -1,22 +1,19 @@
 print ("Please input weight and height, press -9 to stop")
-n = str(input('Please input the name '))
-w = float(input('Please input the weight (kg) '))
-h = float(input('Please input the height (m) '))
 bmi_set = {}
 
 while True:
-  bmi = round(w/(h**2), 1)
-  bmi_set[n] = bmi
   n = str(input('Please input the name '))
   if (n == '-9'):
      break
-  w = float(input('Please input the weight (公斤) '))
+  w = float(input('Please input the weight (kg) '))
   if (w == -9):
      break
-  h = float(input('Please input the height (公尺) '))
+  h = float(input('Please input the height (m) '))
   if (h == -9):
      break
+  bmi = round(w/(h**2), 1)
+  bmi_set[n] = bmi
 
 print ('------')
 for k,v in bmi_set.items():
- print ('The BMI of {} is {}'.format(k, v))
+  print ('The BMI of {} is {}'.format(k, v))
